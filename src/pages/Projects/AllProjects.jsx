7f8cd123735010472bbfbd8b67b78efcaf25602b
Amine-Triki <amine.triki15@gmail.com>
2023-11-27 19:27:36 +0100

    first commit

---
+++ src/pages/Projects/AllProjects.jsx
@@ -0,0 +1,79 @@
+import Img10 from "./../../assets/10.webp"
+import Img9 from "./../../assets/9.webp"
+import Img8 from "./../../assets/8.webp"
+import Img7 from "./../../assets/7.webp"
+import Img6 from "./../../assets/6.webp"
+import Img5 from "./../../assets/5.webp"
+import Img4 from "./../../assets/4.webp"
+import Img3 from "./../../assets/3.webp"
+import Img2 from "./../../assets/2.webp"
+
+const projects = [
+    {
+      category: 'React',
+      title: 'Weather Website',
+      description: 'React + vite + BOOTSTRAP + PWA',
+      link: 'https://amine-triki.github.io/wather-react-pwa/',
+      imageSrc: Img10,
+    },
+    {
+      category: 'Vue-js',
+      title: 'Weather Website',
+      description: 'Vue js + vite + BOOTSTRAP',
+      link: 'https://amine-triki.github.io/wather-vuejs/',
+      imageSrc: Img9,
+    },
+    {
+      category: 'JavaScript',
+      title: 'Weather Website',
+      description: 'JavaScript + BOOTSTRAP',
+      link: 'https://amine-triki.github.io/Weather-js/',
+      imageSrc: Img8,
+    },
+    {
+      category: 'Vue-js',
+      title: 'personal website',
+      description: 'Vue js + tailwind css',
+      link: 'https://amine-triki.github.io/vue-tailwind/',
+      imageSrc: Img7,
+    },
+    {
+      category: 'Vue-js',
+      title: 'personal website',
+      description: 'Vue js + BOOTSTRAP',
+      link: 'https://amine-triki.github.io/vuejs-bootstrap/',
+      imageSrc: Img6,
+    },
+    {
+      category: 'Wordpress',
+      title: 'personal website',
+      description: 'WORDPRESS',
+      link: 'https://aminetriki.com.tn/',
+      imageSrc: Img5,
+    },
+    {
+      category: 'Wordpress',
+      title: 'M.J.S.E website',
+      description: 'WORDPRESS',
+      link: 'https://mjse.com.tn/',
+      imageSrc: Img4,
+    },
+    {
+      category: 'JavaScript',
+      title: 'personal website',
+      description: 'JavaScript + BOOTSTRAP',
+      link: 'https://amine-triki.github.io/portfolio/',
+      imageSrc: Img3,
+    },
+    {
+      category: 'JavaScript',
+      title: 'personal website',
+      description: 'JavaScript + BOOTSTRAP',
+      link: 'https://amine-triki.github.io/',
+      imageSrc: Img2,
+    },
+    
+    // Add more projects as needed
+  ];
+
+  export default projects;
